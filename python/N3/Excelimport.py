de7d3ed2199d81bcb308e4d1b23b1444f1919af5
dvthang <dvthang2906@gmail.com>
2023-12-14 14:46:38 +0900

    file excel thêm dữ liệu vào bảng vocabulary từ N5~N2, N1 chưa có vocabulary.

--- python/N3/Excelimport.py
+++ python/N3/Excelimport.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import mysql.connector
 from sqlalchemy import create_engine
 
 # Thay thế các giá trị này bằng thông tin cơ sở dữ liệu MySQL của bạn
@@ -8,34 +7,13 @@ db_user = "3tpan"
 db_password = "ecc"
 db_name = "3tpandb"
 
-# Tên tệp Excel và tên sheet
-excel_file = "N3-vocabulary.xlsx"
-sheet_name = "Sheet1"
-
-# Tạo kết nối MySQL
-db_connection = mysql.connector.connect(
-    host=db_host, user=db_user, password=db_password, database=db_name
-)
-
 # Tạo đối tượng Engine cho MySQL
 engine = create_engine(
-    f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:3306/{db_name}"
+    f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}/{db_name}"
 )
 
 # Đọc dữ liệu từ tệp Excel vào DataFrame
-df = pd.read_excel(excel_file, sheet_name=sheet_name)
-
-# Loại bỏ các hàng có giá trị NA trong cột 'stt'
-df = df.dropna(subset=["stt"])
-
-# Ép kiểu cho cột 'stt' thành int
-if "stt" in df.columns:
-    df["stt"] = df["stt"].astype(int)
-
+df = pd.read_excel("N3-vocabulary.xlsx", sheet_name="Sheet1")
 
 # Lưu DataFrame vào MySQL
 df.to_sql(name="vocabulary", con=engine, if_exists="append", index=False)
-
-
-# Đóng kết nối MySQL
-db_connection.close()
--- resources/views/home/listen/listen.blade.php
+++ resources/views/home/listen/listen.blade.php
@@ -6,19 +6,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Listen</title>
+    <!-- Thêm CSS của Video.js -->
+    <link href="https://vjs.zencdn.net/7.10.2/video-js.css" rel="stylesheet" />
 
+    <!-- Thêm JavaScript của Video.js -->
+    <script src="https://vjs.zencdn.net/7.10.2/video.min.js"></script>
 
+    <style>
+        .vjs-text-track-display div {
+            position: absolute;
+            top: 10%;
+            /* Điều chỉnh giá trị này để thay đổi vị trí */
+        }
+
+        /* Tùy chỉnh nút play */
+        .vjs-big-play-button {
+            /* Thêm các thuộc tính CSS tại đây */
+        }
+
+        /* Tùy chỉnh thanh tiến trình */
+        .vjs-progress-control {
+            /* Thêm các thuộc tính CSS tại đây */
+        }
+    </style>
 </head>
 
 <body>
-    <video controls width="720">
-        <source src="{{ asset('video/Hàng xóm của tôi là Totoro.mp4') }}" type="video/mp4">
-        <track label="VIETNAMESE" kind="subtitles" srclang="vn"
-            src="{{ asset('video/1988.My.Neighbor.Totoro.BluRay.720p.DTS.3Audio.x264-CHD [co hieu ung, bai hat tieng Anh].vtt') }}"
-            default>
-        Your browser does not support the video tag or the file format of this video.
+
+    <video id="my_video_1" class="video-js vjs-default-skin" controls preload="auto" width="640" height="264"
+        data-setup='{}'>
+        <source src="path_to_your_video.mp4" type="video/mp4">
+        <!-- Thêm phụ đề tại đây -->
+        <track kind="captions" src="path_to_your_subtitle.vtt" srclang="en" label="English">
     </video>
 
+    <script>
+        var player = videojs('my_video_1');
+        player.ready(function() {
+            // Tùy chỉnh và thêm các chức năng ở đây
+        });
+
+
+        player.ready(function() {
+            // Ví dụ: Thêm một nút tùy chỉnh
+            var Button = videojs.getComponent('Button');
+            var customButton = videojs.extend(Button, {
+                // Tùy chỉnh nút tại đây
+            });
+
+            player.addChild(new customButton());
+        });
+    </script>
 </body>
 
 </html>
