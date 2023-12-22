d999c9f6bbe0e9e3b7c64d6f12a6c811b9a05071
dvthang <dvthang2906@gmail.com>
2023-12-23 02:33:02 +0900

    thêm css listenindex. thêm cột images vào cơ sở dữ liệu .

--- resources/views/home/listen/indexListen.blade.php
+++ resources/views/home/listen/indexListen.blade.php
@@ -6,23 +6,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ListenView</title>
+    <style>
+        p {
+            margin-top: 10px;
+        }
+    </style>
 </head>
 
 <body>
     <h1><a href="{{ route('home') }}">HOME</a>&nbsp;video List</h1>
 
     {{-- dữ liệu lấy ra từ database --}}
-    @foreach ($Videolist as $videos)
-        <div>
-            <a href="{{ route('listen-id', ['id' => $videos->id]) }}">{{ $videos->id }}</a>
-            <p>{{ $videos->title }}</p>
-            <p>{{ $videos->description }}</p>
-            {{-- <p>{{ $videos->url }}</p> --}}
-            <p>{{ $videos->updated_at }}</p>
-        </div>
-    @endforeach
+    <div class="videos-container" style="display: flex; flex-wrap: wrap; justify-content: center;">
+        @foreach ($Videolist as $videos)
+            <div class="videoList"
+                style="border: 1px solid black; padding: 10px; width: 230px; height: 260px; text-align: center; margin: 10px;">
+                <a href="{{ route('listen-id', ['id' => $videos->id]) }}">
+                    <!-- Thiết lập kích thước cố định cho ảnh -->
+                    <img src="{{ asset($videos->images ?? 'images/logo.jpg') }}" alt="images"
+                        style="width: 230px; height: 180px;">
+                    <p>{{ $videos->title }}</p>
+                    <p>{{ $videos->description }}</p>
+                    {{-- <p>{{ $videos->url }}</p> --}}
+                    {{-- <p>{{ $videos->updated_at }}</p> --}}
+                </a>
+            </div>
+        @endforeach
+    </div>
     {{-- dữ liệu lấy ra từ database --}}
-
 
 </body>
 
