fb61a146defd9eb1bce62de181a1968492f253fc
dvthang <dvthang2906@gmail.com>
2024-01-15 11:39:46 +0900

    update insertest.admin

--- app/Http/Controllers/admin/AdminNewsController.php
+++ app/Http/Controllers/admin/AdminNewsController.php
@@ -190,7 +190,9 @@ class AdminNewsController extends Controller
             'table_name' => 'required',
         ]);
 
-        $scriptPath = "C:\\Users\\2210314\\Documents\\3tpan\\python\\test_mondai.py";
+        // $scriptPath = "C:\\Users\\2210314\\Documents\\3tpan\\python\\test_mondai.py";
+        $scriptPath = storage_path('app/python/test_mondai.py');
+
 
         if (!file_exists($scriptPath)) {
             return response()->json(['error' => 'Script không tìm thấy'], 404);
